2b63c7787452c86db323f076d72540801e410c33
adripower <adriantorres.gc@gmail.com>
2024-07-12 01:10:49 +0100

    modified:   Images/Basic/ejerciciosMios.js

--- Images/Basic/ejerciciosMios.js
+++ Images/Basic/ejerciciosMios.js
@@ -18,7 +18,7 @@ console.log(edad)
 modificador(edad)
 
 //----------------------------------------
-// te mira el largo del array arreglo y te imprime por consola cada uno de los elementos
+// ! te mira el largo del array arreglo y te imprime por consola cada uno de los elementos
 let arreglo =[1,2,3,4,5];
 for( let i=0;i<arreglo.length; i++){
     let numero=arreglo[i];
@@ -28,20 +28,26 @@ for( let i=0;i<arreglo.length; i++){
 }
 
 //------------------------------------
-//esta no se si esta bien del todo, el metodo map que te mira el largo del array y te lo 
-//La función map se utiliza para recorrer cada elemento del array original y copiarlo en el nuevo array.
+//! esta no se si esta bien del todo, el metodo map que te mira el largo del array y te lo 
+//! La función map se utiliza para recorrer cada elemento del array original y copiarlo en el nuevo array.
 var numero=[2,4,6,8,1,3];
 var numerito =numero.map(function(x){
     return x;
 })
 console.log(numerito)
-//Por ejemplo, si queremos crear un nuevo array que contenga el cuadrado de cada número del array original, podemos utilizar la siguiente función callback:
+//! Por ejemplo, si queremos crear un nuevo array que contenga el cuadrado de cada número del array original, podemos utilizar la siguiente función callback:
 function cuadrado(x) {
     return x * x;
   }
   
   var numerosCuadrados = numero.map(cuadrado);
   console.log(numerosCuadrados); // [4, 16, 36, 64, 1, 9]
+//-----------------------------
+let numero1 =1;
+let numero2 ="2";
+let numero3 =true;
 
 
-  
+console.log(typeof(numero1));
+console.log(typeof(numero2));
+console.log(typeof(numero3));
