da28e2edce5a7ee847e049bfb719475d5f06f728
adripower <adriantorres.gc@gmail.com>
2024-07-17 16:08:50 +0100

    modified:   mios/ejerciciosMios.js

--- mios/ejerciciosMios.js
+++ mios/ejerciciosMios.js
@@ -12,9 +12,9 @@
 
 //!2 se debera hacer obligatoriamente con decremento en la expresion final con saltos de 1
 
-// for(i=0;i>=20;i--){
+// for(i=10;i>=-0;i--){
 //     document.write("el codigo es:" + i+ "<br>")
-// }malllll
+//  }
 
 //!3 el valor del incicializador deve ser 10
 // for (e=10;e<=20;e++){
@@ -22,15 +22,53 @@
 // }
 
 //!4 el valor de 0 se debe saltar
-for(i=1;i<=20;i++){
-    document.write("si esta bien :"+ i + "<br>")
-}
+// for(i=1;i<=20;i++){
+//     document.write("si esta bien :"+ i + "<br>")
+// }
 
 //!5 el bucle tiene que terner un rango de 30 iteraciones aunque se iteren 21 y se muestren 20 en el documento
+// for(i=0;i<=30;i++){
+//     if (i>= 10){
+//         document.write("aqui van la iteraciones" + i + "<br>")
+//     }
+// }
 
 
 
+//!6 el bucle debera mostrar al usuario con un alert() que se va a iniciar.
+//! imprimira los valores de i y avisara al usuario de que el bucle ha finalizado con un document.write()
+// alert("se va a iniciar")
+// for(i=0;i<=2;i++){
+    
+//     document.write("aqui esta la trallada de datos"+ i + "<br>")
+// }
 
-//!6 el bucle debera mostrar al usuario con un alert() que se va a iniciar. imprimira los valores de i y avisara al usuario de que el bucle ha finalizado con un document.write()
+
 
 //!7 se requerira un usuario y contraseña para poder ejecitar el bucle
+
+// let nombre= "adrian";
+// let clave = "1234";
+// let introduceNomobre;
+// let introduceClave;
+
+// introduceNomobre= prompt("instroduce tu nombre")
+// introduceClave = prompt("pon tu clave")
+
+// if(introduceNomobre!= nombre || introduceClave!=clave){
+//     alert("algo esta mal puesto")
+// }
+// else {
+//     alert("se va a iniciar")
+
+// for(i=0;i<=2;i++){
+    
+//     document.write("aqui esta la trallada de datos"+ i + "<br>")
+// }
+   
+
+
+// }
+
+
+
