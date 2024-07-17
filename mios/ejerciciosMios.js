667bb96c9d1ac5fb2aeafab707225d788994dba8
adripower <adriantorres.gc@gmail.com>
2024-07-17 15:17:30 +0100

    modified:   mios/ejerciciosMios.js

--- mios/ejerciciosMios.js
+++ mios/ejerciciosMios.js
@@ -1,4 +1,36 @@
-for(i=1; i<=10; i ++){
-    document.write("aqui se repetira los numeros " + i +"<br>");
-    console.log(i)
+
+//03:36
+
+//!1 que muestre el valor de la variable en el documento 20 veces exactamente
+
+// for (i=0; i <=20;i++){
+//     document.write("aqui va el codigo "+ i+ "<br>")
+// }
+// ok
+
+
+
+//!2 se debera hacer obligatoriamente con decremento en la expresion final con saltos de 1
+
+// for(i=0;i>=20;i--){
+//     document.write("el codigo es:" + i+ "<br>")
+// }malllll
+
+//!3 el valor del incicializador deve ser 10
+// for (e=10;e<=20;e++){
+//     document.write("aqui empieza la fiesta "+e+"<br>")
+// }
+
+//!4 el valor de 0 se debe saltar
+for(i=1;i<=20;i++){
+    document.write("si esta bien :"+ i + "<br>")
 }
+
+//!5 el bucle tiene que terner un rango de 30 iteraciones aunque se iteren 21 y se muestren 20 en el documento
+
+
+
+
+//!6 el bucle debera mostrar al usuario con un alert() que se va a iniciar. imprimira los valores de i y avisara al usuario de que el bucle ha finalizado con un document.write()
+
+//!7 se requerira un usuario y contraseña para poder ejecitar el bucle
