af21cec4ca2575f071922afb3c852e1dab544aee
adripower <adriantorres.gc@gmail.com>
2024-07-18 01:09:33 +0100

    modified:   mios/ejerciciosMios01.js

--- mios/ejerciciosMios01.js
+++ mios/ejerciciosMios01.js
@@ -1,1 +1,42 @@
-function suma(num1, num2)
+let operacion = prompt("¿Qué operación desea? 1-Sumar 2-Restar 3-Multiplicar 4-Dividir");
+let operando1 = parseInt(prompt("Introduce el primer número:"));
+let operando2 = parseInt(prompt("Introduce el segundo número:"));
+let resultado;
+
+function suma(numero1, numero2){
+	return numero1 + numero2;
+}
+function resta(numero1, numero2){
+    return numero1 - numero2;
+}
+function multiplica(numero1 , numero2){
+    return numero1 * numero2;
+}
+function divide(numero1, numero2){
+    return numero1 / numero2;
+}
+switch (operacion){
+	case "1":
+		resultado = suma(operando1,operando2);
+		document.write("El valor de la suma es: " + resultado);
+		break;
+    case "2":
+        resultado = resta(operando1,operando2)
+        document.write("el valor de la resta es: "+ resultado);
+            
+        break;
+    case "3":
+        resultado = multiplica(operando1,operando2)
+        document.write("el valor de la multiplicacion es: "+ resultado);
+            
+        break;
+    case "4":
+        resultado = divide(operando1,operando2)
+        document.write("el valor de la division es: "+ resultado);
+                
+        break;
+
+    default:
+        alert(" algo esta mal puesto");
+        break;
+}
