80962eb7ff981ae43a9197d6532dfe4ec07c6abe
adripower <adriantorres.gc@gmail.com>
2024-07-19 16:03:40 +0100

    new file:   mios/test.css

---
+++ mios/ejerciciosMios02.js
@@ -0,0 +1,14 @@
+//objetos
+let nombre= "Paco"
+
+let usuario ={
+    //propiedades
+    nombre:"adrian",
+    edad: 45,
+    ciudad : "las palmas",
+    telefonos: [343434343, 89879871],
+    idiomas: ["españo","ingles","frances",],
+};
+console.log(usuario.telefonos[1])
+console.log(nombre);
+console.log(usuario.nombre);
