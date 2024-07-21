710d1f30c9be28f156ec646cb9b3abe737ecadfd
adripower <adriantorres.gc@gmail.com>
2024-07-21 15:52:48 +0100

    modified:   mios/ejerciciosMios02.js

--- mios/ejerciciosMios02.js
+++ mios/ejerciciosMios02.js
@@ -1,4 +1,6 @@
-//objetos
+//!objetos
+
+
 let nombre= "Paco"
 
 let usuario ={
@@ -6,9 +8,20 @@ let usuario ={
     nombre:"adrian",
     edad: 45,
     ciudad : "las palmas",
-    telefonos: [343434343, 89879871],
+    telefonos: {
+        telefono1 : 343434343, 
+        telefono2 :89879871},
     idiomas: ["españo","ingles","frances",],
 };
-console.log(usuario.telefonos[1])
+
 console.log(nombre);
 console.log(usuario.nombre);
+console.log(usuario.telefonos.telefono1)
+
+usuario.edad = 40;
+let newEdad =usuario.edad;
+console.log(usuario.edad)
+delete usuario.edad;
+console.log(newEdad)
+
+//05:03:00  cap38
